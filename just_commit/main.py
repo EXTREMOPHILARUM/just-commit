8dc701207b7949128ba9ce4fc731c59b128b6fe2
Saurabh <me@saurabhn.com>
2024-07-01 21:17:10 +0530

    remove main stuff

--- just_commit/main.py
+++
@@ -1,7 +0,0 @@
-# This is the main script logic for the just_commit package
-
-def main():
-    print("This is the main functionality of the just_commit package.")
-
-if __name__ == "__main__":
-    main()
--- tests/test_main.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from just_commit.main import main
-
-class TestMainFunctionality(unittest.TestCase):
-    def test_main_output(self):
-        self.assertIsNone(main(), "The main function should return None")
-
-if __name__ == '__main__':
-    unittest.main()
